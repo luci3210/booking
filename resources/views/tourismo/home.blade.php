20656166766648e05bd7ba8654b0386299baec07
hanscarreon <hanscarreon0898@gmail.com>
2021-06-04 17:36:03 +0800

    mobile nav icons like klook

--- resources/views/tourismo/home.blade.php
+++ resources/views/tourismo/home.blade.php
@@ -3,7 +3,69 @@
 
 <!-- <div class="marg-header"></div> -->
 @section('banner')
-
+<div class="uk-width-1-1  p-20 block-l hide-xl mt-5">
+  <div class="uk-card uk-card-body uk-card-default uk-large uk-width-1-1@l center-item uk-flex">
+      <!-- /. desktop -->
+      <div id="mobile" class="block-l hide-xl center-item" uk-grid>
+        <div class="block-m hidden-xl center-item pd-xs-0">
+          <div class="uk-card item-icon  ">
+            <figure>
+              <img src="{{ asset('image/destination/hotel-icon.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Hotel</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon  ">
+            <figure>
+              <img class="figure-img desc-two" src="{{ asset('image/destination/tour-icon.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Tour Package</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-flight.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Flight</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon ">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/cruise-icon.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Cruise</figcaption>
+            </figure>
+          </div>
+        </div>
+        <!-- /. upper -->
+        <div class="block-m hidden-xl center-item pd-xs-0">
+          <div class="uk-card item-icon">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/events-icon.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Mice Events</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon  ">
+            <figure>
+              <img class="figure-img " src="{{ asset('image/destination/launchpad-icon-tourist-stop.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Tourist Stop</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon ">
+            <figure>
+              <img class="figure-img desc-two" src="{{ asset('image/destination/launchpad-icon-tour-guide.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Tour Guide</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon ">
+            <figure>
+              <img class="figure-img desc-two" src="{{ asset('image/destination/launchpad-icon-translator.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Language Translator</figcaption>
+            </figure>
+          </div>
+        </div>
+        <!-- lower -->
+      </div>
+      <!-- /. mobile -->
+  </div>
+</div>
+<!-- /.mobile nav  -->
 <div class="uk-position-relative uk-visible-toggle uk-light mt-sm-slider" tabindex="-1" uk-slideshow="ratio: 10:3; animation: push">
     <ul class="uk-slideshow-items min-vh-30">
       @foreach($banner as $list)
@@ -26,130 +88,8 @@
 
 @section('content')
 
-<!-- <div class="uk-width-1-1 top-20 p-20">
-<div class="uk-card uk-card-body uk-card-default uk-large uk-width-1-1@l center-item uk-flex">
-    <div class="center-item hidden-m">
-      <div class="uk-card item-icon  uk-card-body">
-        <figure>
-          <img src="{{ asset('image/destination/hotel-icon.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Hotel</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon  uk-card-body ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/tour-icon.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Tour Package</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon  uk-card-body ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-flight.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Flight</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon  uk-card-body ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/cruise-icon.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Cruise</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon  uk-card-body ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/events-icon.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Mice Events</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon  uk-card-body ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-tourist-stop.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Tourist Stop</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon  uk-card-body ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-tour-guide.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Tour Guide</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon  uk-card-body ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-translator.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Language Translator</figcaption>
-        </figure>
-      </div>
-    </div>
-    <div id="mobile" class="block-l hide-xl center-item" uk-grid>
- -->      
 
-      <!-- /. desktop -->
-
-<!-- 
-    <div class="block-m hidden-xl center-item pd-xs-0">
-      <div class="uk-card item-icon  ">
-        <figure>
-          <img src="{{ asset('image/destination/hotel-icon.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Hotel</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon  ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/tour-icon.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Tour Package</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-flight.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Flight</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/cruise-icon.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Cruise</figcaption>
-        </figure>
-      </div>
-    </div>
-    
-
-    /. upper -->
-
-
-    <!-- <div class="block-m hidden-xl center-item pd-xs-0">
-      <div class="uk-card item-icon">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/events-icon.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Mice Events</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon  ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-tourist-stop.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Tourist Stop</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-tour-guide.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Tour Guide</figcaption>
-        </figure>
-      </div>
-      <div class="uk-card item-icon ">
-        <figure>
-          <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-translator.png') }}"alt="my img"/>
-          <figcaption class="icon-title">Language Translator</figcaption>
-        </figure>
-      </div>
-    </div>
- -->
-
-   <!-- lower -->
-    <!-- /. mobile -->
-
-    <!-- </div>
-</div>
-</div>
- -->
+ 
 
 
 <!-- 
---
+++ resources/views/tourismo/navigation_layout_backup.php
@@ -0,0 +1,112 @@
+<div class="uk-width-1-1 top-20 p-20">
+  <div class="uk-card uk-card-body uk-card-default uk-large uk-width-1-1@l center-item uk-flex">
+      <div class="center-item hidden-m">
+        <div class="uk-card item-icon  uk-card-body">
+          <figure>
+            <img src="{{ asset('image/destination/hotel-icon.png') }}"alt="my img"/>
+            <figcaption class="icon-title">Hotel</figcaption>
+          </figure>
+        </div>
+        <div class="uk-card item-icon  uk-card-body ">
+          <figure>
+            <img class="figure-img" src="{{ asset('image/destination/tour-icon.png') }}"alt="my img"/>
+            <figcaption class="icon-title">Tour Package</figcaption>
+          </figure>
+        </div>
+        <div class="uk-card item-icon  uk-card-body ">
+          <figure>
+            <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-flight.png') }}"alt="my img"/>
+            <figcaption class="icon-title">Flight</figcaption>
+          </figure>
+        </div>
+        <div class="uk-card item-icon  uk-card-body ">
+          <figure>
+            <img class="figure-img" src="{{ asset('image/destination/cruise-icon.png') }}"alt="my img"/>
+            <figcaption class="icon-title">Cruise</figcaption>
+          </figure>
+        </div>
+        <div class="uk-card item-icon  uk-card-body ">
+          <figure>
+            <img class="figure-img" src="{{ asset('image/destination/events-icon.png') }}"alt="my img"/>
+            <figcaption class="icon-title">Mice Events</figcaption>
+          </figure>
+        </div>
+        <div class="uk-card item-icon  uk-card-body ">
+          <figure>
+            <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-tourist-stop.png') }}"alt="my img"/>
+            <figcaption class="icon-title">Tourist Stop</figcaption>
+          </figure>
+        </div>
+        <div class="uk-card item-icon  uk-card-body ">
+          <figure>
+            <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-tour-guide.png') }}"alt="my img"/>
+            <figcaption class="icon-title">Tour Guide</figcaption>
+          </figure>
+        </div>
+        <div class="uk-card item-icon  uk-card-body ">
+          <figure>
+            <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-translator.png') }}"alt="my img"/>
+            <figcaption class="icon-title">Language Translator</figcaption>
+          </figure>
+        </div>
+      </div>
+      <!-- /. desktop -->
+      <div id="mobile" class="block-l hide-xl center-item" uk-grid>
+        <div class="block-m hidden-xl center-item pd-xs-0">
+          <div class="uk-card item-icon  ">
+            <figure>
+              <img src="{{ asset('image/destination/hotel-icon.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Hotel</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon  ">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/tour-icon.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Tour Package</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-flight.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Flight</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon ">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/cruise-icon.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Cruise</figcaption>
+            </figure>
+          </div>
+        </div>
+        <!-- /. upper -->
+        <div class="block-m hidden-xl center-item pd-xs-0">
+          <div class="uk-card item-icon">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/events-icon.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Mice Events</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon  ">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-tourist-stop.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Tourist Stop</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon ">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-tour-guide.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Tour Guide</figcaption>
+            </figure>
+          </div>
+          <div class="uk-card item-icon ">
+            <figure>
+              <img class="figure-img" src="{{ asset('image/destination/launchpad-icon-translator.png') }}"alt="my img"/>
+              <figcaption class="icon-title">Language Translator</figcaption>
+            </figure>
+          </div>
+        </div>
+        <!-- lower -->
+      </div>
+      <!-- /. mobile -->
+  </div>
+</div>
