9ee19f42ad89c4b33529f5153a781dadf6a16641
hanscarreon <hanscarreon0898@gmail.com>
2021-06-26 14:43:55 +0800

    grid type

--- resources/views/tourismo/services/index.blade.php
+++ resources/views/tourismo/services/index.blade.php
@@ -14,12 +14,12 @@
   <h2><b>{{ $data[0]->name }} </b><span style="font-size: 15px;padding-left: 25px;"><a href="{{ route('all_services',$data[0]->description) }}" class="uk-link"> View All</a></span></h2>
 </div>
 
-<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
-<ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-5@m uk-grid">
-
+<div class="row ">
 @foreach($data as $list)
 
-<li>
+<div class="col-lg-3 col-md-4 col-sm-4">
+
+
 <div class="icon-box icon-box-pink">
 
 <div class="uk-panel">
@@ -55,16 +55,11 @@
   </div>
 
 </div>
-</li>
-
-@endforeach
         
-    </ul>
-
-    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
 
 </div>
+@endforeach
+
 
     </div>
   </div>
