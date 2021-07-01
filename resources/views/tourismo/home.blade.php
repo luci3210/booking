3ce0272151116919187473abe54761502a697581
jayson <jayson.claros@gmail.com>
2021-07-01 13:19:08 +0800

    Update

--- resources/views/tourismo/home.blade.php
+++ resources/views/tourismo/home.blade.php
@@ -144,6 +144,92 @@
 
 
 
+
+
+
+
+<section class="services team aos-init aos-animate " data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+  <div class="container" style="margin-top: -45px;">
+    <div class="row">
+
+    <div class="section-title">
+      <h2>
+        <b>Tourismo Exclusive  </b> 
+          <span style="font-size: 15px;padding-left: 25px;">
+            <a href="{{ route('destination') }}" class="uk-link"><i class="fas fa-chevron-right"></i> 
+                Explore all {{  $number_of_distination->count() }} Destination
+            </a>
+          </span>
+      </h2>
+    </div>
+
+<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider>
+    <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-5@m uk-grid">
+
+        @foreach($exclusive_packages as $list)
+        <li>
+          <div class="icon-box icon-box-pink">
+
+  <div class="uk-panel">
+      <img src="{{ asset('image/tour/2021')}}/{{ $list->photo == '' ? 'default.png' : $list->photo }}" alt=""  style="border-radius: 4px;">
+      <div class="uk-position-center uk-panel"> </div>
+  </div>
+
+              <div class="member-info">
+
+                <p class="mem-title">{{ substr($list->tour_name, 0, 15) }} ...</p>
+
+                <span>
+                  <i class="fas fa-map-signs"></i> P{{ $list->price }}
+                </span><br>
+
+                <span>
+                  <i class="fas fa-building"></i> {{ substr($list->company, 0, 15) }} ...
+                </span><br>
+
+                <div class="mem-button">
+                  <a class="uk-button uk-button-small btn-room-details-m mb-sm-1" href="{{ route('provice', $list->id) }}">
+                    Explore
+                  </a>
+
+                  <a class="uk-button uk-button-small mb-sm-1" href="javascript:void(0)"uk-toggle="target: #prov-{{$list->id}}" >
+                   Share
+                  </a>
+                </div>
+
+              </div>
+
+          </div>
+      </li>
+      @endforeach  
+  </ul>
+
+<a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+<a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+</div>
+
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <section class="services team aos-init aos-animate " data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
   <div class="container" style="margin-top: -45px;">
     <div class="row">
