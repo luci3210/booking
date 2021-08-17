c547a578023e3579b5ba3d56c40742b1cb752574
unknown <hanscarreon0898@gmail.com>
2021-08-18 00:52:53 +0800

    add auth token in login api

--- app/Http/Controllers/mobile/UserAuthController.php
+++ app/Http/Controllers/mobile/UserAuthController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 use App\Services\UserAuthService;
 use App\Http\Requests\user\CreateUserRequest;
 
+// tools
+use Illuminate\Support\Faceds\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+use App\Model\Api\UsersModel;
+
 
 class UserAuthController extends Controller
 {
@@ -39,5 +46,34 @@ class UserAuthController extends Controller
     public function update_profile(Request $req){
     }
     public function update_password(Request $req){
+    }
+
+
+    public function login_user(Request $req)
+    {   
+        $response = [
+            'success_flag'=>false,
+            'data'=> null,
+            'message'=>null
+
+        ];
+        $email = $this->clean_input($req->email);
+        $password = $this->clean_input($req->password);
+        $validator = Validator::make($req->input(), [ 
+            'email' => ['required', 'email', ],
+            'password'=> ['required', 'string'],
+         ]);
+        if($validator->fails()){
+            $response['message'] = $validator->errors();
+            return response($response, 403);
+        }
+
+        $user= UsersModel::where('email', $req->email)->first();
+        $token = $user->createToken('my-app-token')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response, 201);
     }
 }
