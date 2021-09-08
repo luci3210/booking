29604a5308d8c22df23c6900fc14107fc7d27f51
unknown <hanscarreon0898@gmail.com>
2021-09-08 22:09:12 +0800

    by distrcit

--- resources/views/tourismo/by_district.blade.php
+++ resources/views/tourismo/by_district.blade.php
@@ -41,10 +41,24 @@
   .uk-modal-body {
     border-radius: 4px;
   }
+  @media (min-width: 200px) { 
+    .wd-5{
+      width: 50%;
+      flex: none;
+    }
+
+  }
+  @media (min-width: 768px) { 
+    .wd-5{
+      width: 20%;
+      flex: none;
+    }
+
+  }
   
 </style>
 
-<section class="services team aos-init aos-animate" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500" style="margin-top: 35px !important;">
+<section class="services team aos-init aos-animate vh-95" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500" style="margin-top: 35px !important;">
   <div class="container">
     <div class="row">
 
@@ -53,49 +67,47 @@
 </div>
 
 @foreach($bydistrict as $list)
-<div class="col-md-6 col-lg-2 d-flex align-items-stretch aos-init aos-animate" data-aos="fade-up">
-  <div class="icon-box icon-box-pink">
-  
-    <div class="member mb-0">
-
-      <div class="member-img">
+<div class="wd-5">
+    <div class="icon-box icon-box-pink">
+      <div class="uk-panel">
         <a  href="{{ route('by_name',[$list->description,$list->country,$list->district,$list->tour_name]) }}">
-        <img src="/image/cover/2021/{{ $list->cover == '' ? 'default.png' : $list->cover }}" class="img-fluid" alt="">
+            <img src="{{ asset('image/tour/2021')}}/{{ $list->photo == '' ? 'default.png' : $list->photo }}" alt=""  style="border-radius: 4px;">
+          <div class="uk-position-center uk-panel"> </div>
         </a>
       </div>
 
-      <div class="member-info">
-        <div style="height:70px;">
-            <span>
-            <a href="{{ route('by_name',[$list->description,$list->country,$list->district,$list->tour_name]) }}" class="mem-title title-package" >{{ $list->tour_name }}</a>
-            </span>
-            <span style="margin-top: -8px;font-size: 12px;color:#5f5e5e"><i class="fas fa-star"></i> | 0 Reviews</span>
-            <span class="col-sm text-left" style="color:#f6412d;font-size: 14px;font-weight: 650">₱ {{ $list->price }}</span>
-        </div>
-      </div>
-      <div class="row g-1 px-1 my-2">
-        <div class="col-6">
-          <div class="d-grid gap-2">
-            <a class="uk-button uk-button-small btn-room-details-m mb-sm-1 theme-btn" href="{{ route('by_name',[$list->description,$list->country,$list->district,$list->tour_name]) }}">
-              Explore
-            </a>
+        <div class="member-info">
+
+        <p class="mem-title title-package elips-1 mb-0" uk-tooltip="title: {{ $list->tour_name }}; pos: top-left" title="{{ $list->tour_name }}">{{ $list->tour_name }}</p>
+
+          <span class="text-price  mb-0">
+            <div class="currency-symbol">₱</div> {{ $list->price }}
+          </span><br>
+
+          <div class="row g-1 px-1 my-2">
+            <div class="col-6">
+              <div class="d-grid gap-2">
+                <a class="uk-button uk-button-small theme-btn mb-sm-1" href="{{ route('by_name',[$list->description,$list->country,$list->district,$list->tour_name]) }}">
+                  Explore
+                </a>
+              </div>
+            </div>
+            <!-- /.col -->
+            <div class="col-6">
+              <div class="d-grid gap-2">
+                <a class="uk-button uk-button-small mb-sm-1 theme-btn" href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#global-share"  onclick="openShare('{{$list}}')"  >
+                Share
+                </a>
+              </div>
+            </div>
+            <!-- /.col -->
           </div>
+          <!-- row -->
+
         </div>
-        <!-- /.col -->
-        <div class="col-6">
-          <div class="d-grid gap-2">
-            <a class="uk-button uk-button-small mb-sm-1 theme-btn" href="javascript:void(0)"uk-toggle="target: #prov-{{$list->upload_id}}">
-            Share
-            </a>
-          </div>
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- row -->
 
     </div>
-  </div>
-</div>
+    </div>
 @endforeach
 
     </div>
--- resources/views/tourismo/mobile/by_district_mobile.blade.php
+++ resources/views/tourismo/mobile/by_district_mobile.blade.php
@@ -5,21 +5,173 @@
 <!-- Add the slick-theme.css if you want default styling -->
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
 <style>
-    #main {
-        margin-top: -40px!important;
+#main{
+    margin-top: -40px!important;
+}
+.title-sections{
+    font-size: 1rem;
+    font-weight: 700;
+    margin-bottom: 10px;
+    padding-bottom: 10px;
+    position: relative;
+}
+.shadow-cards{
+    border-radius: 4px;
+    margin: .5rem .5rem;
+    border-radius: 10px;
+    box-shadow: 0px 5px 5px 0px rgba(90,90,90,0.5);
+    -webkit-box-shadow: 0px 5px 5px 0px rgba(90,90,90,0.5);
+    -moz-box-shadow: 0px 5px 5px 0px rgba(90,90,90,0.5);
+}
+.elips{
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: inline;
+}
+
+/* // fold devices (200px and up) */
+@media (min-width: 200px) { 
+    .title-tour{
+        margin-top: 10px;
+        padding: .2rem .2rem;
+        font-size: 1rem!important;
+        line-height: 10px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
     }
-    .text-price {
-        color:#ff2f00 !important;
-        font-size: 12px !important;
-        font-weight: 700 !important;
+    .review-tour{
+        padding: .2rem .2rem;
+        line-height: 10px;
+        font-size: .8rem!important;
+        font-weight: 400!important;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
     }
+    .book-tour{
+        padding: .2rem .2rem;
+        line-height: 10px;
+        font-size: .8rem!important;
+        font-weight: 400!important;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+    .badge-floats{
+        position: absolute;
+        left: 5px;
+        top: 12.5rem;
+        width: 5rem;
+    }
+    .ex-holder{
+        height: 13rem;
+    }
+
+}
+/* // xs devices (320px and up) */
+@media (min-width: 320px) { 
+    .title-tour{
+        margin-top: 10px;
+        padding: .2rem .2rem;
+        font-size: 1rem!important;
+        line-height: 10px;
+    }
+    .review-tour{
+        padding: .2rem .2rem;
+        line-height: 10px;
+        font-size: .7rem!important;
+        font-weight: 400!important;
+    }
+    .book-tour{
+        padding: .2rem .2rem;
+        line-height: 10px;
+        font-size: .7rem!important;
+        font-weight: 400!important;
+    }
+    .badge-floats{
+        position: absolute;
+        left: 5px;
+        top: 12.5rem;
+        width: 5rem;
+    }
+    .ex-holder{
+        height: 12rem;
+    }
+    
+    
+}
+/* // sm devices (360px and up) */
+@media (min-width: 360px) { 
+    .title-tour{
+        margin-top: 10px;
+        padding: .2rem .2rem;
+        font-size: 1rem!important;
+        line-height: 10px;
+    }
+    .review-tour{
+        padding: .2rem .2rem;
+        line-height: 10px;
+        font-size: .7rem!important;
+        font-weight: 400!important;
+    }
+    .book-tour{
+        padding: .2rem .2rem;
+        line-height: 10px;
+        font-size: .7rem!important;
+        font-weight: 400!important;
+    }
+    .badge-floats{
+        position: absolute;
+        left: 5px;
+        top: 12.5rem;
+        width: 5rem;
+    }
+    .ex-holder{
+        height: 13rem;
+    }
+    
+}
+
+/* // Medium devices (411px and up) */
+@media (min-width: 414px) { 
+    .title-tour{
+        margin-top: 10px;
+        padding: .2rem .2rem;
+        font-size: 1rem!important;
+        line-height: 10px;
+    }
+    .review-tour{
+        padding: .2rem .2rem;
+        line-height: 10px;
+        font-size: .7rem!important;
+        font-weight: 400!important;
+    }
+    .book-tour{
+        padding: .2rem .2rem;
+        line-height: 10px;
+        font-size: .7rem!important;
+        font-weight: 400!important;
+    }
+    .badge-floats{
+        position: absolute;
+        left: 5px;
+        top: 12.5rem;
+        width: 5rem;
+    }
+    .ex-holder{
+        height: 14rem;
+    }
+    
+}
 </style>
 @section('content')
 
 
 <section class="services team aos-init aos-animate" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500" >
   <div class="container">
-    <div class="row">
+    <div class="row g-1">
         <div class="col-12">
             <div class="section-title">
                 <h2><b><a href="{{ route('destination') }}" class="uk-link"> </a>{{ $bydistrict[0]->country }} - {{ $bydistrict[0]->district }} </b> <span style="font-size: 15px;padding-left: 25px;"><a href="{{ route('tour_operator') }}" class="uk-link"><i class="fas fa-chevron-right"></i> Explore our Rooms and Packages</a></span></h2>
@@ -28,47 +180,26 @@
         <!-- /.col -->
         @foreach($bydistrict as $list)
         <div class="col-6">
-            <div class="icon-box icon-box-pink">
-    
-              
-
-                <div class="member-img">
-                    <a  href="{{ route('by_name',[$list->description,$list->country,$list->district,$list->tour_name]) }}">
-                    <img src="/image/cover/2021/{{ $list->cover == '' ? 'default.png' : $list->cover }}" class="img-fluid" alt="">
-                    </a>
+        <div class=" shadow-cards">
+            <a  href="{{ route('by_name',[$list->description,$list->country,$list->district,$list->tour_name]) }}">
+                <div class="uk-card-media-top">
+                    <div class="ex-holder" style="width: 100%; 
+                    background-size: cover;
+                    background-position: center;
+                    background-repeat: no-repeat;
+                    background-image: url('{{ asset('image/tour/2021')}}/{{ $list->photo == '' ? 'default.png' : $list->photo }}');
+                    border-top-left-radius: 10px!important;
+                    border-top-right-radius: 10px!important;"></div>
                 </div>
-                <div class="member-info">
-                    <div >
-                        <a href="{{ route('by_name',[$list->description,$list->country,$list->district,$list->tour_name]) }}" class="mem-title title-package" >{{ $list->tour_name }}</a><br>
-                        <span style="margin-top: -8px;font-size: 12px;color:#5f5e5e"><i class="fas fa-star"></i> | 0 Reviews</span>
-                        <p class="text-price text-left my-0" style="color:#f6412d">₱ {{ $list->price }}</p>
-                    </div>
+                <div class="px-2 pb-1">
+                    <h3 class="title-tour text-nowrap mb-0 ">{{ $list->tour_name }}</h3>
+                    <h3 class="review-tour text-nowrap mb-0 mt-0"><i class="fas fa-star text-warning"></i>{{ $list->upload_id + 1 }} <span class="text-muted">(reviews 3,2320)</span></h3>
+                    <h3 class="book-tour text-nowrap  mt-0 text-dark">800k+ Booked</h3>
                 </div>
-                <!-- /.mem info -->
-                <div class="row g-1 px-1 my-2">
-                    <div class="col-6">
-                    <div class="d-grid gap-2">
-                        <a class="uk-button uk-button-small btn-room-details-m mb-sm-1 theme-btn" href="{{ route('by_name',[$list->description,$list->country,$list->district,$list->tour_name]) }}">
-                        Explore
-                        </a>
-                    </div>
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-6">
-                    <div class="d-grid gap-2">
-                        <a class="uk-button uk-button-small mb-sm-1 theme-btn" href="javascript:void(0)"uk-toggle="target: #prov-{{$list->upload_id}}">
-                        Share
-                        </a>
-                    </div>
-                    </div>
-                    <!-- /.col -->
-                </div>
-                <!-- row -->
-               
-            </div>
-            <!-- /.pink box -->
+            </a>
         </div>
-        <!-- /.col -->
+        </div>
+        
         @endforeach
     </div>
   </div>
